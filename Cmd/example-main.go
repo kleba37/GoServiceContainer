b66e7787ccbd321c89e7f3285faf80d04f50c5d9
user <kleba37@yandex.ru>
2025-11-12 20:58:27 +0300

    feat: First Realisation Service Container

---
+++ Cmd/example-main.go
@@ -0,0 +1,3 @@
+package Cmd
+
+import _ "testing"
---
+++ internal/DI/DI.go
@@ -0,0 +1,51 @@
+package DI
+
+import (
+	//"database/sql"
+	//"fmt"
+	"github.com/kleba/Container/internal/Container"
+	//"os"
+
+	//"github.com/joho/godotenv"
+	_ "modernc.org/sqlite"
+)
+
+var autorun = map[string]Container.Service{
+	//"DB": db(),
+}
+
+type DI struct{}
+
+func (DI *DI) New() *Container.Container {
+	container := Container.NewContainer()
+
+	for _, ser := range autorun {
+		container.Register(ser)
+	}
+
+	return container
+}
+
+// Для autoload
+//func db() *sql.DB {
+//	err := godotenv.Load()
+//	if err != nil {
+//		return nil
+//	}
+//
+//	dsn := os.Getenv("DB_DSN")
+//
+//	fmt.Println("DSN: ", dsn)
+//	if len(dsn) == 0 {
+//		panic("DSN invalid")
+//	}
+//
+//	pool, err := sql.Open(os.Getenv("DB_CONNECTION"), dsn)
+//
+//	if err != nil {
+//		fmt.Println("DB error connection")
+//		panic(err)
+//	}
+//
+//	return pool
+//}
